2a36df86c8399aa5e7d2be9529cb6c51560b4fe2
= <=>
2025-05-02 10:17:21 +0530

    initial commit

--- Daily.py
+++ Daily.py
@@ -5,5 +5,12 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('Daily.csv')
 print(df.head(10))
 
+#Copying :-
+
+deep deep
+print('dee')
+
 print(df.info())
 print(df.describe())
+
+print(df['Date'])
